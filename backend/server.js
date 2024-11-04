e3ce1483b8f80cf0edf76eb9b484c1bc36760474
SatyamDevGenie <satyamsawant54@gmail.com>
2024-11-05 03:48:12 +0530

    mongoDB Atlas Connected Successfully

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ import connectDB from './config/db.js';  // Ensure you add the .js extension
 import chalk from "chalk";
 
 
+const app = express();
+
 // Load environment variables
 dotenv.config();
 connectDB();
 
 // Middleware
 app.use(express.json());
-
-const app = express();
 
 app.get("/", (req, res)=>{
     res.send("FocusIn API is running")
@@ -22,6 +22,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+        chalk.yellow.bold (`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
     );
   });
