53a7e4b195885ee8ff2470f605ff5bb7a57a4095
SatyamDevGenie <satyamsawant54@gmail.com>
2024-11-17 13:09:39 +0530

    getAllTasks - userLogin ( JWT )

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -44,4 +44,31 @@ const addTask = asyncHandler(async (req, res) => {
   }
 });
 
-export { addTask };
+
+
+
+// @desc Get all tasks for the logged-in user
+// @route GET /api/tasks
+// @access Private
+const getAllTasks = asyncHandler(async (req, res) => {
+  try {
+    // Find all tasks associated with the user
+    const tasks = await Task.find({ userId: req.user._id });
+
+    if (!tasks || tasks.length === 0) {
+      return res.status(404).json({ success: false, message: "No tasks found" });
+    }
+
+    res.status(200).json({ success: true, data: tasks });
+  } catch (error) {
+    console.error("Error fetching tasks:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching tasks. Please try again later.",
+    });
+  }
+});
+
+
+
+export { addTask, getAllTasks };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { addTask } from "../controllers/taskController.js";
+import { addTask, getAllTasks } from "../controllers/taskController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// @route POST /api/tasks
+// @route POST /api/tasks/create
 // @desc Create a new task
 // @access Private
 router.post("/create", protect, addTask);
+
+// @route GET /api/tasks
+// @desc Get all tasks for the logged-in user
+// @access Private
+router.get("/", protect, getAllTasks);
 
 export default router;
